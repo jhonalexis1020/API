7d5e907c399881a43bdbe335b9cb8e6e1790caae
Jhon Alexis <jhonalexis1020@gmail.com>
2021-04-05 07:10:24 -0500

    Init

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Database;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _DataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _DataContext = dataContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _DataContext.Users.ToList();
+            return users;
+        }
+
+
+
+    }
+}
---
+++ API/Database/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Database
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
