1e3a0ff72ce2eadd7e9a8a437589bfb579d620eb
Jhon Alexis <jhonalexis1020@gmail.com>
2021-05-31 07:49:35 -0500

    Navbar Client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'client';
+  title = '';
   Usuarios: any = [];
 
   constructor(private http: HttpClient) { }
